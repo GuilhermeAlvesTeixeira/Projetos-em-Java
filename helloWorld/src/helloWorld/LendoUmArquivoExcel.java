950446cf8b9391858e3ae35bb6f9ae766c05f79c
Guilherme Alves <117122019+GuilhermeAlvesTeixeira@users.noreply.github.com>
2024-03-19 17:15:19 -0300

    Update LendoUmArquivoExcel.java

--- helloWorld/src/helloWorld/LendoUmArquivoExcel.java
+++ helloWorld/src/helloWorld/LendoUmArquivoExcel.java
@@ -11,25 +11,27 @@ public class LendoUmArquivoExcel {
 		
 		String excelFilePath=".\\datafiles\\ExcelFile.xlsx";
 		
-		//Criando um objeto inputStream para abrir armazenar o caminho do arquivo local do Excel
+		//Criando um objeto inputStream para abrir armazenar o caminho do arquivo local do Excel.
 		FileInputStream inputStream = new FileInputStream(excelFilePath);
 		
-		//Criando um Workbook passando InputStream como parâmetro
+		//Criando um Workbook passando InputStream como parâmetro.
 		XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
 		
-		//Criando uma planilha no Excel
+		//Criando uma planilha no Excel.
 		XSSFSheet sheet = workbook.getSheetAt(0);
 		
-
+		//Criando um objeto da classe iterator para percorrer as linhas do Excel.
 		Iterator iterator = sheet.iterator();
 		while(iterator.hasNext()) {
-			
+
+			//Criando um objeto da classe Iterator para percorrer as células do Excel.
 			XSSFRow row = (XSSFRow) iterator.next();	
 			Iterator cellIterator = row.cellIterator();
 			
 			while(cellIterator.hasNext()) {
 				XSSFCell cell = (XSSFCell) cellIterator.next();
-				
+
+				//Criando um Switch Case para printar o conteúdo das células de acordo com seu tipo.
 				switch(cell.getCellType()) {
 				case STRING: System.out.println(cell.getStringCellValue());
 				break;
